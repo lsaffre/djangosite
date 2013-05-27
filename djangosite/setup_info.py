cb555493c017c3c74ef54f22a846e5f9da41fd81
Luc Saffre <luc.saffre@gmx.net>
2013-05-27 18:40:34 +0300

    http://code.google.com/p/lino/source/browse/docs/blog/2013/0527.rst

--- djangosite/setup_info.py
+++ djangosite/setup_info.py
@@ -8,7 +8,7 @@ and possibly some external tools, too.
 SETUP_INFO = dict(
   name = 'djangosite', 
   version = '0.1.4',
-  install_requires = ['atelier==0.0.2','Django','Sphinx','unipath','python_dateutil'],
+  install_requires = ['atelier==0.0.2','Django>=1.5<1.6','Sphinx','unipath','python_dateutil'],
   description = "A server startup signal for Django",
   license = 'Free BSD',
   test_suite = 'tests',
