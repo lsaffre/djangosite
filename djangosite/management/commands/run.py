32cf667183ace5be180876b5362b4d42e010239e
Luc Saffre <luc.saffre@gmx.net>
2013-08-27 06:52:25 +0300

    http://code.google.com/p/lino/source/browse/docs/blog/2013/0827.rst

--- djangosite/management/commands/run.py
+++ djangosite/management/commands/run.py
@@ -1,7 +1,9 @@
 # -*- coding: UTF-8 -*-
+#~ Copyright 2012-2013 by Luc Saffre.
+#~ License: BSD, see LICENSE for more details.
 """
 Execute a standalone Python script after having set up the Django 
-environment. Also modify `sys.args` and `__name__` so that 
+environment. Also modify `sys.args`, `__file__` and `__name__` so that 
 the invoked script sees them as if it had been called directly.
 
 This is yet another answer to the frequently asked Django question
@@ -9,8 +11,9 @@ about how to run standalone Django scripts
 (`[1] <http://stackoverflow.com/questions/4847469/use-django-from-python-manage-py-shell-to-python-script>`__,
 `[2] <http://www.b-list.org/weblog/2007/sep/22/standalone-django-scripts/>`__).
 It is almost the same as redirecting stdin of Django's ``shell`` command 
-(i.e. doing ``python manage.py shell < myscript.py``), but without the disturbing 
-messages from the interactive console.
+(i.e. doing ``python manage.py shell < myscript.py``), 
+but with the possibility of using command line arguments
+and without the disturbing messages from the interactive console.
 
 For example if you have a file `myscript.py` with the following content...
 
@@ -25,8 +28,6 @@ For example if you have a file `myscript.py` with the following content...
   [<Partner: Rumma & Ko OÜ>, ...  <Partner: Charlier Ulrike>, 
   '...(remaining elements truncated)...']
   
-:copyright: Copyright 2012-2013 by Luc Saffre.
-:license: BSD, see LICENSE for more details.
 """
 
 from __future__ import unicode_literals
@@ -44,5 +45,7 @@ class Command(BaseCommand):
         fn = args[0]
         sys.argv = sys.argv[2:]
         globals()['__name__'] = '__main__'
-        execfile(fn,{})
+        globals()['__file__'] = fn
+        execfile(fn,globals())
+        #~ execfile(fn,{})
           
