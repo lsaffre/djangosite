89dca4574c0e878e9deae7c720ae50ffaa152821
Luc Saffre <luc.saffre@gmail.com>
2014-06-10 02:51:18 +0300

    http://docs.lino-framework.org/blog/2014/0610.html

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,20 +3,28 @@ from unipath import Path
 ROOTDIR = Path(__file__).parent.parent
 
 # load  SETUP_INFO:
-execfile(ROOTDIR.child('djangosite','project_info.py'),globals())
+execfile(ROOTDIR.child('djangosite', 'project_info.py'), globals())
 
 from djangosite.utils.pythontest import TestCase
 
+
 class BaseTestCase(TestCase):
     project_root = ROOTDIR
-    
-class BasicTests(BaseTestCase):
-    def test_01(self): 
-        self.assertEqual(1+1,2)
 
-    def test_float2decimal(self): self.run_docs_django_tests('tested.float2decimal.settings')
-    def test_integer_pk(self): self.run_docs_django_tests('tested.integer_pk.settings')
+   
+class BasicTests(BaseTestCase):
+
+    def test_01(self):
+        self.assertEqual(1+1, 2)
+
+    def test_float2decimal(self):
+        self.run_docs_django_tests('tested.float2decimal.settings')
+
+    def test_integer_pk(self):
+        self.run_docs_django_tests('tested.integer_pk.settings')
+
 
 class PackagesTests(BaseTestCase):
-    def test_packages(self): self.run_packages_test(SETUP_INFO['packages'])
+    def test_packages(self):
+        self.run_packages_test(SETUP_INFO['packages'])
 
